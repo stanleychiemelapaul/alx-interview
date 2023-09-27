3b0a975b80552f908f3db18630718b2455c0efca
stanleychiemelapaul <stanleypaul313@gmail.com>
2023-09-27 02:14:07 +0100

    Pascal Triangle Task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) < n:
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for i in range(1, len(prev_row)):
+            new_row.append(prev_row[i-1] + prev_row[i])
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
+
+# Example usage:
+result = pascal_triangle(5)
+for row in result:
+    print(row)
